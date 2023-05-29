eecb311d79401f56f7acef3a09f15c5aead8eed2
Roman <gauzy.gossamer@gmail.com>
2023-05-29 14:29:43 +0300

    typo in BGPUpdate

--- messages.go
+++ messages.go
@@ -26,7 +26,7 @@ type BGPUpdate struct {
     UpdateType string  `json:"type"`
     Origin string      `json:"origin"`
     Path []int         `json:"path"`
-    Commuinty []string `json:"community,omitempty"`
+    Community []string `json:"community,omitempty"`
     Announcements []string `json:"announcements"`
     Withdrawals []string   `json:"withdrawals"`
 }
